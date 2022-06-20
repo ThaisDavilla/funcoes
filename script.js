637803e0e966bcc9d34109ff980bf89a09e9adb8
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-20 09:55:21 -0300

    Exercicio de Função do curso vai na Web.

---
+++ script.js
@@ -0,0 +1,36 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+function saudacao(){
+  console.log("Olá, estou estudando lógica de programação!")
+}
+saudacao()
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+function oi(nome){
+  console.log(`Oi, meu nome é ${nome}.`)
+}
+  oi("Thais")
+
+  //3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+  function ficha(aluno, idade, musica){
+    return(aluno, idade, musica)
+  }
+  console.log("Samuel,", "Desenvolvedor Web,","Rap")
+
+  //4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+  function arte(filme, musica){
+    return(filme, musica)
+  }
+  console.log("Capitão Fantástico,", "O Barquinho (Nara Leão)")
+
+  //5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+  function triplo(number){
+    return number*3
+  }
+  console.log(triplo(5))
+
+
